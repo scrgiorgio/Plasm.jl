1aa5e8825b88073a4562c8924e80eee19a056214
Giorgio <scrgiorgio@sci.utah.edu>
2023-12-05 20:06:12 +0100

    new version

--- test/properties.jl
+++ test/properties.jl
@@ -1,18 +1,46 @@
 using Plasm
-VIEW(CUBE(1),title="normal cube (1)")
 
-VIEW(CUBE(1),"normal cube (2)")
+# different ways to `View`
+VIEW(CUBE(1),title="normal cube (1)")
+VIEW(CUBE(1), "normal cube (2)")
 VIEW(CUBE(1),title="normal cube (13)")
+
+# example: how to set the overall background color
 VIEW(CUBE(1),background_color=[0.8,0.1,0.5],title="cube with background color")
 
-VIEW(PROPERTIES(CUBE(1),Dict("line_width"=>10)),title="line_width")
-VIEW(PROPERTIES(CUBE(1),Dict("line_color"=>[1.0,0.0,0.0])),title="line_color")
-VIEW(PROPERTIES(CUBE(1),Dict("face_color"=>[0.0,1.0,0.0])),title="face_color")
+# line_with property
+VIEW(
+   PROPERTIES(
+      CUBE(1),
+      Dict("line_width"=>10)),
+   title="line_width")
 
-
-VIEW(STRUCT(
-	PROPERTIES(STRUCT(T(1)(0.0),CUBE(1)), Dict("face_color"=>[0.0,1.0,0.0])),
-	PROPERTIES(STRUCT(T(1)(1.0),CUBE(1)), Dict("face_color"=>[1.0,0.0,0.0]))),
-	title="2 colored cubes"
+# line_color property
+VIEW(
+  PROPERTIES(
+     CUBE(1),
+     Dict("line_color"=>[1.0,0.0,0.0])),
+  title="line_color"
 )
 
+# face_color property
+VIEW(
+   PROPERTIES(
+     CUBE(1),
+     Dict("face_color"=>[0.0,1.0,0.0])),
+   title="face_color"
+)
+
+# example of mixing and matching different properties
+VIEW(STRUCT(
+   PROPERTIES(
+      STRUCT(T(1)(0.0),CUBE(1)), 
+      Dict("face_color"=>[0.0,1.0,0.0])
+   ),
+   PROPERTIES(
+      STRUCT(T(1)(1.0),CUBE(1)), 
+      Dict("face_color"=>[1.0,0.0,0.0]))
+   ),
+   title="2 colored cubes",
+   background_color=[0.0,0.0,0.0]
+)
