79977433999f631221822986eff0669c1ba1a58e
apaoluzzi <apaoluzzi@mac.com>
2024-01-11 12:12:29 +0100

    src/complex.jl

--- test/complex.jl
+++ test/complex.jl
@@ -50,6 +50,50 @@ end
 
 # //////////////////////////////////////////////////////////////////////////////
 
+# extracted from alberto's code
+
+"""
+SIMPLEX(1)
+SIMPLEX(2)
+SIMPLEX(3)
+SIMPLEX(4)
+
+CUBOID([1,1])
+CUBOID([1,1,1])
+CUBOID([1,1,1,1])
+
+simplex6 = simplex(6,complex=true)
+simplex6.V
+simplex6.C
+Hpc(simplex6.V, simplex6.C[:C5V])
+
+Simplex6 = Lar(Hpc(simplex6.V, simplex6.C[:C5V]))
+
+obj3 = Hpc(Lar(CUBOID([1,1,1]))); # OK
+VIEW(Hpc(Lar(obj3).V, Lar(obj3).C[:EV])) # OK
+VIEW(Hpc(Lar(obj3).V, Lar(obj3).C[:FV])) # OK
+
+LAR(STRUCT(SQUARE(1), T(1,2)(0.5,0.25), SQUARE(1))) # 2D
+LAR(STRUCT(CUBE(1), T(1,2)(0.5,0.25), CUBE(1))) # 3D |V|=16,|FV|=12,|EV|=24 ... OK!
+LAR(STRUCT(CUBE(1), T(1)(1), CUBE(1))) # |V|=12,|FV|=11,|EV|=20 ... OK!  correct by using LAR ... uncorrect using Lar !!!
+
+cube3 = (LAR∘STRUCT∘CAT∘DIESIS(3))([T(1)(1), CUBE(1)]); # NB:  LAR  !!!
+VIEW(Hpc(cube3.V, cube3.C[:EV])) # OK
+VIEW(Hpc(cube3.V, cube3.C[:FV])) # OK
+
+grid1D = INTERVALS(10.0)(10) # equivalent to the next
+grid1D = QUOTE(DIESIS(10)(1.0)) # equivalent to the former
+
+mesh = CUBOIDGRID([5,5,5])
+VIEW(Hpc(mesh.V, mesh.C[:EV]))
+
+GRID1(5)
+
+mesh = CUBOIDGRID([10,10])
+VIEW(Hpc(mesh.V, mesh.C[:EV])) # problema ai bordi
+VIEW(Hpc(mesh.V, mesh.C[:FV])) # OK ai bordi
+"""
+
 
 
 
