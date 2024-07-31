277d97cc1789faac5e8c5c691aa96b4e34194d47
apaoluzzi <apaoluzzi@mac.com>
2024-07-31 12:03:04 +0200

    new example 3tubes3d.jl

--- test/3tubes3d.jl
+++ test/3tubes3d.jl
@@ -1,19 +1,25 @@
 using Plasm
 
-tube = T(3)(-2)(TUBE([0.5,0.4,4])(4));
+tube = T(3)(-2)(CYLINDER([0.5,4])(4));
 assembly = STRUCT( tube, R(2,3)(π/2), tube, R(1,3)(π/2), tube );
 VIEW(assembly)
 
 obj = LAR(assembly);
 V, (EV, FV) = obj.V, (obj.C[:EV], obj.C[:FV]);
 
-W = permutedims(V);
-KEV = lar2cop(EV);
-KFV = lar2cop(FV);
+V,CVs,FVs,EVs = testarrangement(V,FV,EV)
+show_exploded(V,CVs,FVs,EVs)
 
-KFE = (KFV * KEV') .÷ Int8(2);
-V, copEV, copFE, copCF = space_arrangement(
-   W::Points, KEV::ChainOp, KFE::ChainOp);
-   
-# va in loop durante lo splitting 2D, scrivendo:
-# 1/60
+
+#tube = T(3)(-2)(TUBE([0.5,0.4,4])(4));
+
+#W = permutedims(V);
+#KEV = lar2cop(EV);
+#KFV = lar2cop(FV);
+#
+#KFE = (KFV * KEV') .÷ Int8(2);
+#V, copEV, copFE, copCF = space_arrangement(
+#   W::Points, KEV::ChainOp, KFE::ChainOp);
+#   
+## va in loop durante lo splitting 2D, scrivendo:
+## 1/60
