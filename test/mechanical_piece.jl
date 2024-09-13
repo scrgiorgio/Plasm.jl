33d72ec50c2c7acb85536f3b6b3f587d2094e5b3
Giorgio Scorzelli <scrgiorgio@gmail.com>
2024-09-13 11:48:45 +0200

    new version

---
+++ test/mechanical_piece.jl
@@ -0,0 +1,21 @@
+using Plasm
+using Random
+
+Random.seed!(0)
+
+cyl = T(3)(-2)(CYLINDER([0.5,4])(8))
+
+# TODO use TUBE([0.8,1.0,3.0])
+
+hpc = STRUCT( 
+  STRUCT(
+    T(1,2,3)(-1,-1,-1),
+    CUBE(2)), 
+  cyl, R(2,3)(π/2), 
+  cyl, R(1,3)(π/2), 
+  cyl 
+)
+lar = LAR(hpc)
+
+arrangement = ARRANGE3D(lar)
+VIEWCOMPLEX(arrangement,explode=[1.2,1.2,1.8])
