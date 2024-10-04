438c0f8349392e119aa0eaa24a2c76009010805d
apaoluzzi <apaoluzzi@mac.com>
2024-10-04 21:29:53 +0200

    installement of src/Plasm.jl

--- src/Plasm.jl
+++ src/Plasm.jl
@@ -7,7 +7,7 @@ include("./fenvs.jl")
 include("./arrange.jl")
 include("./complex.jl")
 include("./boolean.jl")
-include("./congruence.jl")
+#include("./congruence.jl")
 include("./lar2triangles.jl")
 include("./atoms.jl")
 
