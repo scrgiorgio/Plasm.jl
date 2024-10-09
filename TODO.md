3366c5cfec37b504e35d8aead6e6e69aa0648388
Giorgio Scorzelli <scrgiorgio@gmail.com>
2024-10-09 09:13:22 +0200

    new version

--- TODO.md
+++ TODO.md
@@ -1,22 +1,22 @@
-# TODO 
+# TODO
 
-## [DONE] (8-12-24) Estendere VIEWCOMPLEX alle celle solide (atomi) quando note e convesse. 
-   In particolare con sintassi tipo:
-   `VIEWCOMPLEX( lar, show=["CV"], explode=[2,2,2] ) # DA FARE`
-   
-## [DONE] (8-12-24) `git merge` (branch paoluzzi): 
-ho scritto due file:
-devi sostituire `SIMPLEX`  (o eliminarlo)  in `fenvs.jl`
-e scommentare i test su `SIMPLEX`.  Ovviamente dovresti inserire i due file nel `main` ...
-
-## [DONE] (6-12-24) Ripristinare SPLIT come l'avevo scritta: ritorna `atoms, outer`
-L'errore che mi ponevi ieri nel 99,99% dei casi **concreti** non si può presentare.  
-
-## [TODO:: will take time ] (6-12-24) Eliminare vicoli di tipo troppo stretti
-
+**Eliminare vicoli di tipo troppo stretti**
 In particolare **tutte** le volte che fissi `::Float64` è meglio che metti `::Number`.
 Prima ho dovuto capire perché l'esempio `TetGen.jl`  mi dava errore in `MKPOL`.
 Motivo:  tu fissavi `Vector{Vector{Int64}}` mentre i dati erano `Vector{Vector{Int32}}` ...
+
+
+
+# DONE 
+
+- Estendere VIEWCOMPLEX alle celle solide (atomi) quando note e convesse. 
+- `git merge` (branch paoluzzi)
+- replace `SIMPLEX` 
+- Ripristinare SPLIT come l'avevo scritta: ritorna `atoms, outer`
+
+
+
+
 
 
 
--- src/Plasm.jl
+++ src/Plasm.jl
@@ -18,9 +18,6 @@ export DEFAULT_LAR_FONT_SIZE
 DEFAULT_VIEWER="glfw"
 export DEFAULT_VIEWER
 
-LAR_DEFAULT_ERR=1e-8
-export LAR_DEFAULT_ERR
-
 include("./config.jl")
 include("./utils.jl")
 include("./geometry.jl")
@@ -30,54 +27,54 @@ include("./defaults.jl")
 include("./viewer.jl")
 include("./hpc.jl")
 include("./fenvs.jl")
-include("./lar/lar.jl")
-include("./lar/classify.jl")
+
+LAR_DEFAULT_ERR=1e-8
+export LAR_DEFAULT_ERR
+
+LAR_ARRANGE_VERSION=1
+export LAR_ARRANGE_VERSION
 
 LAR_ARRANGE2D_ROUND         = 4
 LAR_ARRANGE2D_PERTURBATION  = 1e-4 * 0.01
-
 LAR_ARRANGE2D_SMALL_TRIANGLES_ERR    = 1e-4
 LAR_ARRANGE3D_UNPROJECT_ROUND_DIGITS = 4
 
+include("./lar/lar.jl")
+include("./lar/simplexn.jl")
+include("./lar/classify.jl")
 include("./lar/arrange2d.jl")
 include("./lar/arrange3d.jl")
-
 include("./lar/experimental/arrange2d.jl")
 include("./lar/experimental/arrange3d.jl")
-
 include("./lar/boolean.jl")
-include("./lar/simplexn.jl")
-
-LAR_ARRANGE_VERSION=1
 
 function ARRANGE2D(lar::Lar; debug_mode=false)::Lar
 	return LAR_ARRANGE_VERSION==2 ? arrange2d_v2(lar) : arrange2d_v1(lar)
 end
+export ARRANGE2D
 
 function ARRANGE3D(lar::Lar; debug_mode=false)::Lar
 	return LAR_ARRANGE_VERSION==2 ? arrange3d_v2(lar,debug_mode=debug_mode) : arrange3d_v1(lar)
 end
+export ARRANGE3D
 
 function INNERS(lar::Lar)::Lar
 	return LAR_ARRANGE_VERSION==2 ? arrange3d_v2_inners(lar) : arrange3d_v1_inners(lar)
 end
+export INNERS
 
 function OUTERS(lar::Lar)::Lar
 	return LAR_ARRANGE_VERSION==2 ? arrange3d_v2_outers(lar) : arrange3d_v1_outers(lar)
 end
-
+export OUTERS
 
 function SPLIT(lar::Lar)::Lar
 	inners=INNERS(lar)
 	outers =OUTERS(lar)
 	@assert(length(outers)==1) # backward compatibility for alberto
 	return inners,outer[1] 
-
-export LAR_ARRANGE_VERSION
-export ARRANGE2D
-export ARRANGE3D
-export INNERS
-export OUTERS
+end
+export SPLIT
 
 function __init__()
 	InitPythonHullCode()
